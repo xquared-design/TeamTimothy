a300f2807d06110bc5f9bdad75a0ba2ec01f1b0d
xquared <xquared@contrapposto.vanhack.ca>
2017-02-22 18:01:24 -0800

    working on dicts of button mapping

--- Code/GPIOint.py
+++ Code/GPIOint.py
@@ -1,33 +1,55 @@
 import RPi.GPIO as GPIO
 import time
+import threading
 GPIO.setmode(GPIO.BCM)
+
+DictIn = {1:17,2:27,3:22,4:5,5:6}
 
 GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(22, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(18,GPIO.OUT,initial=GPIO.HIGH)
-GPIO.setup(23,GPIO.OUT,initial=GPIO.HIGH)
+GPIO.setup(18, GPIO.OUT, initial=GPIO.HIGH)
+GPIO.setup(23, GPIO.OUT, initial=GPIO.HIGH)
 
-def callback_btn17(channel):
-    GPIO.output(18, GPIO.LOW)
-    print "btn1 pressed, activating 18"
-    GPIO.output(18, GPIO.HIGH)
+GPIO.setup(21, GPIO.OUT, initial=GPIO.LOW) #setup power button
 
-def callback_btn27(channel):
-    GPIO.output(23, GPIO.LOW)
-    print "btn2 pressed, activating 23"
-    GPIO.output(23, GPIO.HIGH)
+comboLookup = {17:[18],27:[23],22:[18,23]}
 
-def callback_btn22(channel):
-    GPIO.output(18, GPIO.LOW)
-    GPIO.output(23, GPIO.LOW)
-    print "btn3 pressed, activating both 18 and 23"
-    GPIO.output(18, GPIO.HIGH)
-    GPIO.output(23, GPIO.HIGH)
+def thread_outputMacro(pin):
+    GPIO.output(comboLookup[pin],GPIO.LOW)
+    print "pulling",  "low"
+    time.sleep(0.25)
+    GPIO.output(comboLookup[pin],GPIO.HIGH)
+    print "returning",  "high"
 
-GPIO.add_event_detect(27, GPIO.FALLING, callback=callback_btn27,bouncetime=500)
-GPIO.add_event_detect(17, GPIO.FALLING, callback=callback_btn17,bouncetime=500)
-GPIO.add_event_detect(22, GPIO.FALLING, callback=callback_btn22,bouncetime=500)
+def callback_btnPress(pin):
+    #GPIO.output(18, GPIO.LOW)
+    print "btn %d pressed" % pin
+    t=threading.Thread(target=thread_outputMacro, args=(pin,),name="macroThread")
+    t.start()
+    #GPIO.output(18, GPIO.HIGH)
 
-while True:
-    time.sleep(1e6)
+#def callback_btn27(channel):
+#    GPIO.output(23, GPIO.LOW)
+#    print "btn2 pressed, activating 23"
+#    GPIO.output(23, GPIO.HIGH)
+#
+#def callback_btn22(channel):
+#    GPIO.output(18, GPIO.LOW)
+#    GPIO.output(23, GPIO.LOW)
+#    print "btn3 pressed, activating both 18 and 23"
+#    GPIO.output(18, GPIO.HIGH)
+#    GPIO.output(23, GPIO.HIGH)
+
+GPIO.add_event_detect(27, GPIO.FALLING, callback=callback_btnPress,bouncetime=500)
+GPIO.add_event_detect(17, GPIO.FALLING, callback=callback_btnPress,bouncetime=500)
+GPIO.add_event_detect(22, GPIO.FALLING, callback=callback_btnPress,bouncetime=500)
+
+GPIO.output(21,GPIO.HIGH)
+
+try:
+    while True:
+        time.sleep(1e6)
+except KeyboardInterrupt:
+    GPIO.cleanup()       # clean up GPIO on CTRL+C exit
+GPIO.cleanup()           # clean up GPIO on normal exit
