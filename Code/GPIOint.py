1aae0d086a869293ebdaeda692407c5d1dc90aba
xquared <xquared@contrapposto.local>
2017-03-10 17:22:15 -0500

    updated pin mapping for output

--- Code/GPIOint.py
+++ Code/GPIOint.py
@@ -1,19 +1,21 @@
 import RPi.GPIO as GPIO
 import time
 import threading
+import os
 GPIO.setmode(GPIO.BCM)
 
 DictIn = {1:17,2:27,3:22,4:5,5:6}
-DictOut = {'A':18,'B':23}
+DictOut = {'A':18,'B':23,'C':24,'Z':25}
 
-for key in DictIn
+for key in DictIn:
     GPIO.setup(DictIn[key], GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-for btn in DictOut
-    GPIO.setup(DictOut(btn), GPIO.OUT, initial=GPIO.HIGH)
+for key in DictOut:
+    GPIO.setup(DictOut[key], GPIO.OUT, initial=GPIO.HIGH)
 
 
-GPIO.setup(21, GPIO.OUT, initial=GPIO.LOW) #setup power button
+GPIO.setup(21, GPIO.OUT, initial=GPIO.LOW) #setup power on indicator light
+GPIO.setup(26, GPIO.IN, pull_up_down=GPIO.PUD_UP) #setup shutdown btn
 
 comboLookup = {17:[18],27:[23],22:[18,23]}
 
@@ -31,6 +33,10 @@ def callback_btnPress(pin):
     t.start()
     #GPIO.output(18, GPIO.HIGH)
 
+def callback_btnShutdown(pin):
+    GPIO.cleanup()           # clean up GPIO on shutdown
+    os.system("sudo shutdown -h now")
+
 #def callback_btn27(channel):
 #    GPIO.output(23, GPIO.LOW)
 #    print "btn2 pressed, activating 23"
@@ -43,9 +49,10 @@ def callback_btnPress(pin):
 #    GPIO.output(18, GPIO.HIGH)
 #    GPIO.output(23, GPIO.HIGH)
 
-GPIO.add_event_detect(27, GPIO.FALLING, callback=callback_btnPress,bouncetime=500)
-GPIO.add_event_detect(17, GPIO.FALLING, callback=callback_btnPress,bouncetime=500)
-GPIO.add_event_detect(22, GPIO.FALLING, callback=callback_btnPress,bouncetime=500)
+for key in DictIn:
+    GPIO.add_event_detect(DictIn[key], GPIO.FALLING, callback=callback_btnPress,bouncetime=500)
+
+GPIO.add_event_detect(26, GPIO.RISING, callback=callback_btnShutdown, bouncetime=500)
 
 GPIO.output(21,GPIO.HIGH)
 
@@ -54,4 +61,4 @@ try:
         time.sleep(1e6)
 except KeyboardInterrupt:
     GPIO.cleanup()       # clean up GPIO on CTRL+C exit
-GPIO.cleanup()           # clean up GPIO on normal exit
+
