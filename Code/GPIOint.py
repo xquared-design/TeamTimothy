bf57dae70be4db74437c0603fceb89c296c23aeb
xquared <xquared@contrapposto.vanhack.ca>
2017-04-08 15:00:43 -0700

    mapped out the combo for CZ
    added method for detecting button release that doesn't do anything right now: "callback_btnRelease"

--- Code/GPIOint.py
+++ Code/GPIOint.py
@@ -4,7 +4,9 @@ import threading
 import os
 GPIO.setmode(GPIO.BCM) #use BCM numbering scheme
 
-#map buttons to BCM pins per PinOut.md
+
+
+
 DictIn = {1:17,2:27,3:22,4:5,5:6,6:26}
 DictOut = {'A':18,'B':23,'C':24,'Z':25}
 
@@ -15,11 +17,12 @@ for key in DictOut:
     GPIO.setup(DictOut[key], GPIO.OUT, initial=GPIO.HIGH)
 
 
-GPIO.setup(21, GPIO.OUT, initial=GPIO.LOW) #setup power on indicator light
-GPIO.setup(26, GPIO.IN, pull_up_down=GPIO.PUD_UP) #setup shutdown button
 
-#map BCM button inputs to BCM Wiimote outputs
-comboLookup = {17:[18],27:[23],22:[18,23],5:[18,23],6:[25],26:[24]}  #button 6/GPIO26 (on joystick) mapped to C
+#map buttons to BCM pins per PinOut.md
+GPIO.setup(20, GPIO.OUT, initial=GPIO.LOW) #setup power on indicator light
+GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_UP) #setup shutdown btn
+
+comboLookup = {17:[18],27:[23],22:[18,23],5:[25],6:[24]}
 
 #when an input pin is called, toggle the corresponding output pin ON (LOW) and OFF
 def thread_outputMacro(pin):
@@ -35,6 +38,11 @@ def callback_btnPress(pin):
     t=threading.Thread(target=thread_outputMacro, args=(pin,),name="macroThread")
     t.start()
     #GPIO.output(18, GPIO.HIGH)
+
+def callback_btnRelease(pin):
+    print "btn %d released" % pin
+
+
 
 def callback_btnShutdown(pin):
     GPIO.cleanup()           # clean up GPIO on shutdown
@@ -53,11 +61,12 @@ def callback_btnShutdown(pin):
 #    GPIO.output(23, GPIO.HIGH)
 
 for key in DictIn:
-    GPIO.add_event_detect(DictIn[key], GPIO.FALLING, callback=callback_btnPress,bouncetime=500)
+    GPIO.add_event_detect(DictIn[key], GPIO.FALLING, callback=callback_btnPress,bouncetime=300)
+    GPIO.add_event_detect(DictIn[key], GPIO.RISING, callback=callback_btnRelease,bouncetime=300)
 
-GPIO.add_event_detect(26, GPIO.RISING, callback=callback_btnShutdown, bouncetime=500)
+GPIO.add_event_detect(16, GPIO.FALLING, callback=callback_btnShutdown, bouncetime=300)
 
-GPIO.output(21,GPIO.HIGH)
+GPIO.output(20,GPIO.HIGH)
 
 try:
     while True:
